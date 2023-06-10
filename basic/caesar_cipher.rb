8ec1f4c597e9c31f030062a58d705cb75cbd5210
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-06-10 16:17:05 -0300

    refactor: visual refactor to code

--- basic/caesar_cipher.rb
+++ basic/caesar_cipher.rb
@@ -1,9 +1,10 @@
 
-# Implements a caesar cipher that takes in a string and the shift factor and then outputs the modified string
+# Implements a caesar cipher that takes in a string and the shift factor and 
+# then outputs the modified string:
 
 def caesar_cipher(plaintext, position)
   ascii = plaintext.bytes
-  modif_string = ""
+  modified_string = ""
 
   for ascii_value in ascii
     if ascii_value >= 65 && ascii_value <= 90
@@ -12,12 +13,11 @@ def caesar_cipher(plaintext, position)
     if ascii_value >= 97 && ascii_value <= 122
       ascii_value = (ascii_value + position - 97) % (122 - 97 + 1) + 97
     end
-    modif_string += ascii_value.chr
+    modified_string += ascii_value.chr
   end
 
-  modif_string
+  modified_string
 end
 
-encrypted_text = caesar_cipher("What a string!", 5)
-puts(encrypted_text)
+puts caesar_cipher("What a string!", 5)
 
