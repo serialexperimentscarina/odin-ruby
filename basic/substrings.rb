15cd34aa088d1c5744e81687c201bbd395dca70e
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-06-10 14:53:33 -0300

    feat: substrings

---
+++ basic/substrings.rb
@@ -0,0 +1,23 @@
+
+# Implement a method #substrings that takes a word as the first argument and then an array of valid substrings 
+# (your dictionary) as the second argument. 
+# It should return a hash listing each substring (case insensitive) that was found in the original string and 
+# how many times it was found.
+
+def substrings(string, dictionary)
+  listing = {}
+  string.downcase!
+
+  for word in dictionary
+    if string.include?(word)
+      occurences = string.scan(word).length
+      listing[word] =  occurences if occurences > 0
+    end
+  end
+  listing
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+puts substrings("below", dictionary)
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
